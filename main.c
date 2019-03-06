51578d22c1b974c4a6c6fbaeb102ec32be6c3cd5
Alex Khomich <alexkh90@gmail.com>
2019-03-07 01:07:02 +0300

    Replace path to woff file on relative.

--- main.c
+++ main.c
@@ -5,7 +5,7 @@
 #include <stdlib.h>
 
 int main() {
-    const char* font_path = "/mnt/dev/rust/untiltled/fonts/noto-sans-tc/noto-sans-tc.woff";
+    const char* font_path = "../noto-sans-tc.woff";
     size_t decoded_len;
     uint8_t *decoded_data;
     decoded_data = decode_from_file_wrapped(font_path, &decoded_len);
