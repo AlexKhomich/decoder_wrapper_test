76660942664d529404d2c3d1578cb9bd3f49d82d
Alex Khomich <alexkh90@gmail.com>
2019-04-07 20:51:58 +0300

    Update example.

--- main.c
+++ main.c
@@ -6,10 +6,31 @@
 
 int main() {
     const char *font_path = "../noto-sans-tc.woff";
-    Result *result = decode_from_file_wrapped(font_path);
-    if (result->error == DecodeError) {
-        destroy_buffer(result);
+    DecodedResult *result = decode_from_file_wrapped(font_path);
+    size_t buf_len = 0;
+    switch (result->error) {
+        case DecodeError:
+        case DataSourceIsEmpty:
+        case OutBufferFull:
+        case BuffError:
+        case DecompressError:
+        case InvalidWoffSize:
+        case InputBufferIsEmpty:
+        case InvalidWoffSignature:
+        case InvalidWoffStructure:
+        case CreateFileError:
+        case OpenFileError:
+        case WriteToFileError:
+        case ReadFromFileError:
+        case InputPathError:
+        case OutputPathError:
+            destroy_decoded_result(result);
+            printf("failed");
+            break;
+        case None:
+            buf_len = result->decoded_data_len;
+            destroy_decoded_result(result);
+            printf("success");
+            break;
     }
-    size_t buf_len = result->decoded_data_len;
-    destroy_buffer(result);
 }
